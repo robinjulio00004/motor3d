16219788ea7b04b699bc631c74a0b3b7f57a9803
julio00004 <robinjulio004@gmail.com>
2025-08-27 14:14:02 -0600

    Update ccplane.js

--- ccplane.js
+++ ccplane.js
@@ -13,7 +13,7 @@ mi_boton.addEventListener('click',function(){
 		document.getElementById("status").innerHTML = "OFF";
 		mi_boton.value="Vista sección OFF";
 	}
-});
+},true);
 
 AFRAME.registerComponent('ccplane', {
       schema: {
@@ -97,6 +97,7 @@ AFRAME.registerComponent('mi_ccplane',{
     this.el.object3D.add(planeHelper);
   }
 });
+
 
 
 
