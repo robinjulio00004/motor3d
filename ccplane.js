3fd0973fd284672e85deafdf73a6bcaba5459f46
julio00004 <robinjulio004@gmail.com>
2025-08-27 14:18:38 -0600

    Update ccplane.js

--- ccplane.js
+++ ccplane.js
@@ -1,19 +1,7 @@
 let eje=1,offset=0,eboton=false,aclip=false;
 let renderer = document.querySelector("a-scene").renderer;
 
-const mi_boton=document.querySelector(".button");
-mi_boton.addEventListener('click',function(){
-		eboton=!eboton;
-	if(eboton===true)
-	{    
-		document.getElementById("status").innerHTML = "ON";
-		mi_boton.value="Vista sección ON";
-	}else
-	{ 
-		document.getElementById("status").innerHTML = "OFF";
-		mi_boton.value="Vista sección OFF";
-	}
-},true);
+
 
 AFRAME.registerComponent('ccplane', {
       schema: {
@@ -97,6 +85,7 @@ AFRAME.registerComponent('mi_ccplane',{
     this.el.object3D.add(planeHelper);
   }
 });
+
 
 
 
